29a9a57e78cbec218aa5092f205e1f75a6812ddf
Chintapalli S D Raghavendra <dattu046@gmail.com>
2018-06-14 19:17:34 +0530

    added server area

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 // Angular DIST output folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('./dist'));
 
 // API location
 //app.use('/api', api);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile('./dist/index.html');
 });
 
 //Set Port
