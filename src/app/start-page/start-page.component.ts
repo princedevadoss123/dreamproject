dd9c02acbeeb8b1f1a58c30e221c506ae8fe91b9
root <prince.devadoss@sap.com>
2018-06-15 21:07:59 +0530

    Login-Chooser Added

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-start-page',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-page.component.css']
 })
 export class StartPageComponent implements OnInit {
-
-  constructor() { }
+  private urlString: string;
+  constructor (
+    private router: Router) {
+    }
 
   ngOnInit() {
   }
 
+  moveToLoginPage() {
+    this.urlString = '/login-chooser';
+    this.router.navigate([this.urlString]);
+  }
 }
