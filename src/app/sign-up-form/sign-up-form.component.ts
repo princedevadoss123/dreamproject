1c565ceaa306bff1435697f6f17a588473989112
root <prince.devadoss@sap.com>
2018-06-22 16:31:15 +0530

    Registration Validation Done

--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -8,13 +8,46 @@ import { ValidationService } from '../services/validation/validation.service';
 })
 export class SignUpFormComponent implements OnInit {
   model: any = {};
+  emailValidator: boolean = true;
+  passwordValidator:boolean = true;
+  confPasswordValidator:boolean = true;
+  contactValidator:boolean = true;
+  intialModelCheck:boolean = false;
   
   constructor(private validator: ValidationService) { }
 
   ngOnInit() {
   }
 
-  register(event) {
-    console.log(event);
+  passwordChecker() {
+    if(this.intialModelCheck) {
+      this.validator.passwordValidation(this.model.password) ? this.passwordValidator = true : this.passwordValidator = false;
+    } 
+  }
+
+  emailChecker() {
+    if(this.intialModelCheck) {
+      this.validator.emailValidation(this.model.username) ? this.emailValidator = true : this.emailValidator = false;
+    }
+  }
+
+  confPasswordChecker() {
+    if(this.intialModelCheck) {
+      this.validator.confirmPasswordValidation(this.model.password, this.model.confPassword) ? this.confPasswordValidator =true : this.confPasswordValidator = false;
+    }
+  }
+
+  contactChecker() {
+    if(this.intialModelCheck) {
+      this.validator.contactValidation(this.model.contact) ? this.contactValidator =true : this.contactValidator = false;
+    }
+  }
+
+  register() {
+    this.intialModelCheck = true;
+    this.emailChecker();
+    this.passwordChecker();
+    this.confPasswordChecker();
+    this.contactChecker();
   }
 }
