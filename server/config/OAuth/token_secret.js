30e0351d849a48557e6096c9b82f0e39ee8bf762
Chintapalli S D Raghavendra <dattu046@gmail.com>
2018-06-27 21:04:42 +0530

    Added JWT token, and the session is stored with the JWT token instead of user id.

---
+++ server/config/OAuth/token_secret.js
@@ -0,0 +1,5 @@
+const jwtsecret = {
+    key: "178817399504849sgfjdfdafsajksaf"
+}
+
+module.exports = jwtsecret
--- server/routes.js
+++ server/routes.js
@@ -8,10 +8,11 @@ const userverification = require('./SignUp/verify');
 /* Sample Home Url to test the Login Functionality */
 module.exports = function(app){
 
-  app.get('/home', function(request,response){
+  app.get('/success', function(request,response){
 
     if(request.isAuthenticated()){
-      response.redirect('/');
+      response.setHeader("Authorization",request.user);
+      response.redirect('/home');
     }else{
       response.sendStatus(403);
     }
@@ -31,23 +32,23 @@ app.get('/auth/linkedin',
   passport.authenticate('linkedin'));
 
 app.post('/auth/login',
-  passport.authenticate('local', { successRedirect: '/home',
-                                   failureRedirect: '/home',
+  passport.authenticate('local', { successRedirect: '/success',
+                                   failureRedirect: '/',
                                    failureFlash: true })
 );
 
 /* Call back functions for Thirdparty Authentication Mechanisams*/
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/home',
-                                      failureRedirect: '/login' }));
+  passport.authenticate('facebook', { successRedirect: '/success',
+                                      failureRedirect: '/' }));
 app.get('/auth/google/callback',
-  passport.authenticate('google', { successRedirect: '/home',
-                                      failureRedirect: '/login' }));
+  passport.authenticate('google', { successRedirect: '/success',
+                                      failureRedirect: '/' }));
 
 app.get('/auth/linkedin/callback',
-  passport.authenticate('linkedin', { successRedirect: '/home',
-                                      failureRedirect: '/login' }));
+  passport.authenticate('linkedin', { successRedirect: '/success',
+                                      failureRedirect: '/' }));
 
 app.get('/logout', function(req, res){
   req.logout();
